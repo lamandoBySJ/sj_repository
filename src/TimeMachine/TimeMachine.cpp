f4870e0fe0b9b0d9c4c703150a83a2ceae4c57a3
lamandoBySJ <905147369@qq.com>
2021-02-24 10:16:33 +0800

    stlb

---
+++ src/TimeMachine/TimeMachine.cpp
@@ -0,0 +1,40 @@
+
+#include "TimeMachine.h"
+
+//extern  rtos::Mutex _mutex;
+
+TimeMachine::TimeMachine(DS1307& rtc,rtos::Mutex& mutex):_rtc(rtc),_mutex(mutex)
+{
+ 
+}
+
+void TimeMachine::startup(void *pvParameters)
+ {
+    // _mutex.lock();
+     _rtc.begin();
+     _rtc.setEpoch(1610000000);
+     _rtc.setHourMode(CLOCK_H24);
+     _rtc.startClock();
+   // _mutex.unlock();
+ }
+ time_t TimeMachine::getEpoch()
+ {
+     time_t epoch=0;
+    // _mutex.lock();
+     if(_rtc.isRunning()){
+       epoch = _rtc.getEpoch();
+     }
+   // _mutex.unlock();
+    return epoch;
+ }
+
+String TimeMachine::getDateTime()
+ {
+     String datetime="";
+    // _mutex.lock();
+     if(_rtc.isRunning()){
+        datetime = _rtc.getDateTime();
+     }
+    //_mutex.unlock();
+    return datetime;
+ }
---
+++ src/TimeMachine/TimeMachine.h
@@ -0,0 +1,23 @@
+#ifndef TIME_MACHINE_H
+#define TIME_MACHINE_H
+
+#include "Arduino.h"
+#include "DS1307.h"
+#include "rtos/rtos.h"
+
+using namespace rtos;
+class TimeMachine
+{
+public:
+    TimeMachine()=delete;
+    TimeMachine(DS1307& rtc,rtos::Mutex& mutex);
+    ~TimeMachine()=default;
+    void startup(void *pvParameters);
+    time_t getEpoch();
+    String getDateTime();
+private:
+    DS1307& _rtc;
+    rtos::Mutex& _mutex;
+};
+
+#endif
